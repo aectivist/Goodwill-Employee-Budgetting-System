3d32ce643b667d87436461e193ba3a5d5d8378d3
aectivist <165470058+aectivist@users.noreply.github.com>
2024-09-30 20:06:16 +0800

    Update Calculator.py

--- IATEST/Calculator.py
+++ IATEST/Calculator.py
@@ -63,9 +63,8 @@ def show_page(page):
 
 
 
-TitleFont = CTkFont(family="Oswald", size=15, weight='bold')
+OutputCalculatorFont = CTkFont(family="Oswald", size=30, weight='bold')
 EditFont = CTkFont(family="Oswald", size=15, weight='bold')
-BTNFont = CTkFont(family="Oswald", size=13)
 
 
 def salespage(page):
@@ -80,8 +79,10 @@ def salespage(page):
     CalculatorSide.grid(row=0,column=1)
     CalculatorSide.grid_propagate(0)    
     #RIGHT BAR UI
-    OutputCalculations = CTkEntry(CalculatorSide, width=410, height=120, border_color="#000000", border_width=1)
+    OutputCalculations = CTkEntry(CalculatorSide ,font=OutputCalculatorFont, width=410, height=120, border_color="#000000", border_width=1)
     OutputCalculations.grid(row=0,column=0)
+    OutputCalculations.configure(state="readonly")
+
 
     ButtonsForCalculationsFrame = CTkFrame(CalculatorSide, width=410, height=200, border_color="#000000", border_width=1)
     ButtonsForCalculationsFrame.grid(row=1, column=0)
@@ -126,56 +127,74 @@ def salespage(page):
     #I do this because it's funny
     button6 = CTkButton(ButtonsForCalculationsFrame, text="%",height=40, width=60, border_color="#000000", border_width=1, fg_color="#FFFFFF", text_color="#000000")
     button6.grid(row=0, column=3)
-    button7 = CTkButton(ButtonsForCalculationsFrame, text="7",height=40, width=60, border_color="#000000", border_width=1, fg_color="#FFFFFF", text_color="#000000")
+    button7 = CTkButton(ButtonsForCalculationsFrame,command=lambda: appendtoentry("7",OutputCalculations, "integer"), text="7",height=40, width=60, border_color="#000000", border_width=1, fg_color="#FFFFFF", text_color="#000000")
     button7.grid(row=1, column=3)
-    button8 = CTkButton(ButtonsForCalculationsFrame, text="4",height=40, width=60, border_color="#000000", border_width=1, fg_color="#FFFFFF", text_color="#000000")
+    button8 = CTkButton(ButtonsForCalculationsFrame,command=lambda: appendtoentry("4",OutputCalculations, "integer"), text="4",height=40, width=60, border_color="#000000", border_width=1, fg_color="#FFFFFF", text_color="#000000")
     button8.grid(row=2, column=3)
-    button9 = CTkButton(ButtonsForCalculationsFrame, text="1",height=40, width=60, border_color="#000000", border_width=1, fg_color="#FFFFFF", text_color="#000000")
+    button9 = CTkButton(ButtonsForCalculationsFrame,command=lambda: appendtoentry("1",OutputCalculations, "integer"), text="1",height=40, width=60, border_color="#000000", border_width=1, fg_color="#FFFFFF", text_color="#000000")
     button9.grid(row=3, column=3)
-    button0 = CTkButton(ButtonsForCalculationsFrame, text="0",height=40, width=60, border_color="#000000", border_width=1, fg_color="#FFFFFF", text_color="#000000")
+    button0 = CTkButton(ButtonsForCalculationsFrame,command=lambda: appendtoentry("0",OutputCalculations, "integer"), text="0",height=40, width=60, border_color="#000000", border_width=1, fg_color="#FFFFFF", text_color="#000000")
     button0.grid(row=4, column=3)
-    
+    #the long slope of redundancy and idiocracy
     button6 = CTkButton(ButtonsForCalculationsFrame, text="(",height=40, width=60, border_color="#000000", border_width=1, fg_color="#FFFFFF", text_color="#000000")
     button6.grid(row=0, column=4)
-    button7 = CTkButton(ButtonsForCalculationsFrame, text="8",height=40, width=60, border_color="#000000", border_width=1, fg_color="#FFFFFF", text_color="#000000")
+    button7 = CTkButton(ButtonsForCalculationsFrame,command=lambda: appendtoentry("8",OutputCalculations, "integer"), text="8",height=40, width=60, border_color="#000000", border_width=1, fg_color="#FFFFFF", text_color="#000000")
     button7.grid(row=1, column=4)
-    button8 = CTkButton(ButtonsForCalculationsFrame, text="5",height=40, width=60, border_color="#000000", border_width=1, fg_color="#FFFFFF", text_color="#000000")
+    button8 = CTkButton(ButtonsForCalculationsFrame,command=lambda: appendtoentry("5",OutputCalculations, "integer"), text="5",height=40, width=60, border_color="#000000", border_width=1, fg_color="#FFFFFF", text_color="#000000")
     button8.grid(row=2, column=4)
-    button9 = CTkButton(ButtonsForCalculationsFrame, text="2",height=40, width=60, border_color="#000000", border_width=1, fg_color="#FFFFFF", text_color="#000000")
+    button9 = CTkButton(ButtonsForCalculationsFrame,command=lambda: appendtoentry("2",OutputCalculations, "integer"), text="2",height=40, width=60, border_color="#000000", border_width=1, fg_color="#FFFFFF", text_color="#000000")
     button9.grid(row=3, column=4)
     button0 = CTkButton(ButtonsForCalculationsFrame, text=".",height=40, width=60, border_color="#000000", border_width=1, fg_color="#FFFFFF", text_color="#000000")
     button0.grid(row=4, column=4)
 
     button6 = CTkButton(ButtonsForCalculationsFrame, text=")",height=40, width=60, border_color="#000000", border_width=1, fg_color="#FFFFFF", text_color="#000000")
     button6.grid(row=0, column=5)
-    button7 = CTkButton(ButtonsForCalculationsFrame, text="9",height=40, width=60, border_color="#000000", border_width=1, fg_color="#FFFFFF", text_color="#000000")
+    button7 = CTkButton(ButtonsForCalculationsFrame,command=lambda: appendtoentry("9",OutputCalculations, "integer"), text="9",height=40, width=60, border_color="#000000", border_width=1, fg_color="#FFFFFF", text_color="#000000")
     button7.grid(row=1, column=5)
-    button8 = CTkButton(ButtonsForCalculationsFrame, text="6",height=40, width=60, border_color="#000000", border_width=1, fg_color="#FFFFFF", text_color="#000000")
+    button8 = CTkButton(ButtonsForCalculationsFrame,command=lambda: appendtoentry("6",OutputCalculations, "integer"), text="6",height=40, width=60, border_color="#000000", border_width=1, fg_color="#FFFFFF", text_color="#000000")
     button8.grid(row=2, column=5)
-    button9 = CTkButton(ButtonsForCalculationsFrame, text="3",height=40, width=60, border_color="#000000", border_width=1, fg_color="#FFFFFF", text_color="#000000")
+    button9 = CTkButton(ButtonsForCalculationsFrame,command=lambda: appendtoentry("3",OutputCalculations, "integer"), text="3",height=40, width=60, border_color="#000000", border_width=1, fg_color="#FFFFFF", text_color="#000000")
     button9.grid(row=3, column=5)
     button0 = CTkButton(ButtonsForCalculationsFrame, text="=",height=40, width=60, border_color="#000000", border_width=1, fg_color="#FFFFFF", text_color="#000000")
     button0.grid(row=4, column=5)
 
     button6 = CTkButton(ButtonsForCalculationsFrame, text="CE", height=40, width=60, border_color="#000000", border_width=1, fg_color="#FFFFFF", text_color="#000000")
     button6.grid(row=0, column=6)
-    button7 = CTkButton(ButtonsForCalculationsFrame, text="÷",height=40, width=60, border_color="#000000", border_width=1, fg_color="#FFFFFF", text_color="#000000")
+    button7 = CTkButton(ButtonsForCalculationsFrame, text="÷",height=40, width=60 ,command=lambda: appendtoentry("÷",OutputCalculations, "operation"), border_color="#000000", border_width=1, fg_color="#FFFFFF", text_color="#000000")
     button7.grid(row=1, column=6)
-    button8 = CTkButton(ButtonsForCalculationsFrame, text="x",height=40, width=60, border_color="#000000", border_width=1, fg_color="#FFFFFF", text_color="#000000")
+    button8 = CTkButton(ButtonsForCalculationsFrame, text="x",height=40, width=60,command=lambda: appendtoentry("x",OutputCalculations, "operation"),  border_color="#000000", border_width=1, fg_color="#FFFFFF", text_color="#000000")
     button8.grid(row=2, column=6)
-    button9 = CTkButton(ButtonsForCalculationsFrame, text="-",height=40, width=60, border_color="#000000", border_width=1, fg_color="#FFFFFF", text_color="#000000")
+    button9 = CTkButton(ButtonsForCalculationsFrame, text="-",height=40, width=60,command=lambda: appendtoentry("-",OutputCalculations, "operation"),  border_color="#000000", border_width=1, fg_color="#FFFFFF", text_color="#000000")
     button9.grid(row=3, column=6)
-    button0 = CTkButton(ButtonsForCalculationsFrame, text="+",height=40, width=60, border_color="#000000", border_width=1, fg_color="#FFFFFF", text_color="#000000")
+    button0 = CTkButton(ButtonsForCalculationsFrame, text="+",height=40, width=60,command=lambda: appendtoentry("+",OutputCalculations, "operation"),  border_color="#000000", border_width=1, fg_color="#FFFFFF", text_color="#000000")
     button0.grid(row=4, column=6)
 
  
     
     
+#It's morbilations time...
 
-    #CALCULATOR BUTTONS
+def appendtoentry(value, OutputCalculations, datatype):
+    print(value)
+    global expression
+    if datatype == "integer":
+        current_value = OutputCalculations.get()
+        OutputCalculations.configure(state="normal")
+        OutputCalculations.delete(0, END)
+        OutputCalculations.insert(0, current_value + str(value))
+        OutputCalculations.configure(state="readonly")
 
+    elif datatype != "integer" :
+        current_value = OutputCalculations.get()
+        OutputCalculations.configure(state="normal")
+        OutputCalculations.delete(0, END)
+        OutputCalculations.insert(0, current_value + str(value))
+        OutputCalculations.configure(state="readonly")
+        
 
-   
+def HolderFunction():
+    number = []
+    operation = []
 
 
 
